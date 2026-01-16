82dcecd955ae006605977f4875f29a6e750752f2
elmorshedy-del <elmorshedy@icloud.com>
2026-01-16 02:21:38 -0500

    fix: add missing fetchFromApify function definition

--- server/services/apifyService.js
+++ server/services/apifyService.js
@@ -114,6 +114,7 @@ export async function searchByBrand(store, brandName, options = {}) {
 /**
  * Fetch ads from Apify actor
  */
+async function fetchFromApify(searchQuery, options = {}) {
   const { country = 'ALL', limit = 2 } = options;
 
   const searchUrl = `https://www.facebook.com/ads/library/?active_status=active&ad_type=all&country=${country === "ALL" ? "ALL" : country}&q=${encodeURIComponent(searchQuery)}&search_type=keyword_unordered&media_type=all`;
